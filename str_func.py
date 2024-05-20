36b339f7dd64107c75aa6432470c3dd935b5fb62
Hilshat76 <hilshat76@gmail.com>
2024-05-20 14:06:26 +0300

    Исправлен str_func.py, дописана функция str_title() и docstrig

--- str_func.py
+++ str_func.py
@@ -1,2 +1,16 @@
 def str_upper(original_text):
+    """
+    Преобразует все буквы текста в верхний регистр
+    :param original_text: Исходный текст
+    :return: Преобразованный текст
+    """
     return original_text.upper()
+
+
+def str_title(original_text):
+    """
+    Преобразует первые буквы каждого слова в верхний регистр
+    :param original_text: Исходный текст
+    :return: Преобразованный текст
+    """
+    return original_text.title()ext.upper()
